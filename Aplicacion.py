47b152c35e3f42a2b193f9197fe5727282c5efdd
Juan Acosta <73087520+Wembie@users.noreply.github.com>
2020-11-03 10:23:33 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,2 +1,8 @@
-#python
 print("Hola Wembie")
+def menu():
+  print("1. Registrar.")
+  print("2. Buscar paciente")
+  print("3. Borrar paciente")
+  print("4. Ver total pacientes.")
+  print("0. Salir.")
+  
