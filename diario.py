9a08df3e74ce299fcbf425ad1d61562c88358c26
jmlopezriani <73970106+jmlopezriani@users.noreply.github.com>
2020-11-24 08:54:51 -0500

    Update diario.py
    me falta implementar el uso de excel como base de datos efectivamente. Ya está toda la base del programita ahí, con los recursos que necesita para aplicarle el workbook. Sigo viendo como hacer una base de datos dinámica pero que perdure a través de usos, para manetener un registro relativamente largo.

--- diario.py
+++ diario.py
@@ -1,1 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 09:31:29 2020
 
+@author: jmlop
+"""
+
+
+import pandas as pd
+import openpyxl as xl
+import xlsxwriter
+import os.path
+import datetime
+
+
+def menu():
+    veces = 0
+    entrada = {'nombre':[],'fecha':[],'animo':[]}
+    while True:
+        print("          Diario          \n__________________________")
+        print("[1] Crear entrada\n[2] Ver entradas previas\n[3] Ver promedio de ánimo\n[0] terminar")
+        try:
+            p = int(input("Qué desea hacer?\n"))
+        except:
+            p = 0
+        if p >= 0 and p < 4 and (type(p) is int):
+            if p == 1:
+                crearE(veces, entrada)
+                veces += 1
+            elif p == 2:
+                verE()
+            elif p == 3:
+                print(prom(entrada))
+            elif p == 0:
+                break
+            else:
+                print("Ingrese una opción válida")
+
+
+        
+
+
+
+def crearE(x:int, entrada:[]):
+    entrada['nombre'].append(input("Ingrese su nombre completo:\n"))
+    entrada['fecha'].append(datetime.datetime.now())
+    entrada['animo'].append(int(input("Del 1 al 10 ¿Cómo se siente hoy?\n")))
+    excel = pd.DataFrame(entrada)
+    excel.to_excel()
+
+    
+def prom(entrada) :
+    ac = 0
+    if len(entrada['animo']) > 0:
+        for i in entrada['animo']:
+            ac += i
+        ac = ac / len(entrada['animo'])
+        return "El promedio de sus entradas de ánimo es " + str(ac)
+    else:
+        return("No hay registros")
