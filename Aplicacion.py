e242a03a2502b95e75a9014ab1c6d42b1e3ac048
juanjorestrepo <49797614+JuanjoRestrepo@users.noreply.github.com>
2020-12-10 09:30:40 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -957,7 +957,7 @@ if opcionCE == 1:
     print("___ VERSION GRATIS ___")
     while True:
       gratis()
-      opcionGratis = preguntarNumero( 0, 3, "[0,1,2,3]")  
+      opcionGratis = preguntarNumero( 0, 3, "[0,1,2]")  
       if opcionGratis == 1:
         elegirTip()
       if opcionGratis == 2:
